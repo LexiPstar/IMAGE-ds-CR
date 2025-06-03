97a46454ab1074e526027150f99a99b2e3cad8f6
LexiPstar <jianrebeca@outlook.com>
2025-06-04 00:19:38 +0800

    pkl

--- tools/build_vocab.py
+++ tools/build_vocab.py
@@ -4,12 +4,15 @@ from utils.utils import load_config
 import sys
 import os
 
+# 将当前脚本所在目录的上级目录添加到系统路径中，以便能够导入项目中的模块
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def main():
+    # 加载配置文件
     config = load_config("config/config.yaml")
 
-    # 用 get_loader 只为了提取 vocab
+    # 使用 get_loader 函数加载数据，这里主要是为了提取词汇表
+    # 由于我们只需要词汇表，所以忽略前两个返回值（数据加载器和数据集）
     _, _, vocab = get_loader(
         image_folder=config["data"]["image_folder"],
         captions_file=config["data"]["captions_file"],
@@ -18,9 +21,11 @@ def main():
         shuffle=False
     )
 
+    # 将提取的词汇表保存到指定文件中
     with open("data/vocab.pkl", "wb") as f:
         pickle.dump(vocab, f)
 
+    # 打印成功信息
     print("✅ Vocabulary saved to data/vocab.pkl")
 
 
